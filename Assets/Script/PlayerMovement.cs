a32f8a7d1b7720ae2735e7491b24136e682351e1
Hypurine <hrisqulah@gmail.com>
2025-12-17 10:21:18 +0700

    asdasd
    asdasd

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    private Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        movement = movement.normalized;
+
+        bool isMoving = movement.magnitude > 0.01f;
+        animator.SetBool("Running", isMoving);
+
+        if (movement.x < 0)
+            spriteRenderer.flipX = false;
+        else if (movement.x > 0)
+            spriteRenderer.flipX = true;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
